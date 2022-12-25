cf560dc5e5735d35ced6eaa4303b454f06713ea4
kashishdipro <kamanashishdatta@gmail.com>
2022-12-25 21:44:09 +0600

    delete product from db by seller in my products component

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../contexts/AuthProvider';
 import { FaTrashAlt } from 'react-icons/fa';
+import { toast } from 'react-hot-toast';
 
 const MyProducts = () => {
     const {user} = useContext(AuthContext);
-    const {data: products = []} = useQuery({
+    const {data: products = [], refetch} = useQuery({
         queryKey: ['products', user?.email],
         queryFn: async() =>{
             const res = await fetch(`http://localhost:5000/products?seller_email=${user?.email}`);
@@ -13,7 +14,22 @@ const MyProducts = () => {
             return data;
         }
     })
-    console.log(products);
+
+    const handleDelete = id =>{
+        const proceed = window.confirm('Are you sure, you want to delete this product?');
+        if(proceed){
+            fetch(`http://localhost:5000/products/${id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data =>{
+                if(data.deletedCount > 0){
+                    toast.success('Deleted Successfully');
+                    refetch();
+                }
+            })
+        }
+    }
     return (
         <section className='m-4'>
             <h2 className='text-2xl text-neutral font-medium mb-4'>My Products</h2>
@@ -45,7 +61,7 @@ const MyProducts = () => {
                                     </div>
                                 </td>
                                 <td><button className='btn btn-xs btn-outline btn-primary'>{product.status}</button></td>
-                                <td><button className='text-red-700 hover:text-red-900 p-2'><FaTrashAlt/></button></td>
+                                <td><button onClick={() =>handleDelete(product._id)} className='text-red-700 hover:text-red-900 p-2'><FaTrashAlt/></button></td>
                             </tr>)
                         }
                     </tbody>
